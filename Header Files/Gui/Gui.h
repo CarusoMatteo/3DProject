a975fc598acf3eb897a354a4e514613bf32085d7
Matteo Caruso <matteo.caruso7@studio.unibo.it>
2026-08-15 23:23:23 +0200

    Moved inspector screen

--- Header Files/Gui/Gui.h
+++ Header Files/Gui/Gui.h
@@ -27,7 +27,7 @@ public:
 
 private:
 	const fvec2 settingsWindowPosition = fvec2(10, 10);
-	const fvec2 inspectorWindowPosition = fvec2(1000, 10);
+	const fvec2 inspectorWindowPosition = fvec2(10, 470);
 
 	shared_ptr<fvec3> clearColor;
 
